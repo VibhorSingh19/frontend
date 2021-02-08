757584f946db84f33c9f2dea925b505a7fb9aeb7
Vibhor Singh <vibhor.formal@gmail.com>
2021-02-08 14:03:57 +0530

    Fetching data

--- src/App.js
+++ src/App.js
@@ -3,23 +3,15 @@ import './App.css';
 import * as ReactBootStrap from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  /*const players=[
-    {
-      "position":"Forward",name:"Vibhor",team:"IND"
-    },
-    {
-      "position":"Middle",name:"Shubh",team:"INDD"
-    },
-    {
-      "position":"Back",name:"Uttu",team:"INDDD"
-    }
-  ]*/
   const players=[{"SS_id":1001,"SS_Name":"Session1","SS_Duration":112,"SS_Type":"V"},
   {"SS_id":1002,"SS_Name":"Session2","SS_Duration":60,"SS_Type":"V"},
   {"SS_id":1003,"SS_Name":"Session3","SS_Duration":15,"SS_Type":"P"},
   {"SS_id":1004,"SS_Name":"Session4","SS_Duration":120,"SS_Type":"V"},
   {"SS_id":1005,"SS_Name":"Session5","SS_Duration":15,"SS_Type":"P"}]
-   
+  console.log(players); 
+  /*fetch("https://ibm-sprint.herokuapp.com/session")
+      .then(console.log())
+  */
   const renderPlayer=(player,index)=>{
     return(
       <tr key={index}>
