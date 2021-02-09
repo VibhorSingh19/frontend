b395b9c223890ea3fb19f7d165d494f753831bc9
Vibhor Singh <vibhor.formal@gmail.com>
2021-02-09 16:16:17 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -3,26 +3,53 @@ import './App.css';
 import * as ReactBootStrap from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  const players=[{"SS_id":1001,"SS_Name":"Session1","SS_Duration":112,"SS_Type":"V"},
+  /*constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      isLoaded:false,
+    }
+  }
+  componentDidMount(){
+    fetch("https://ibm-sprint.herokuapp.com/session")
+    .then(res=> res.json())
+    .then(json=>{
+      this.setState({
+        isLoaded:true;
+        items:json,
+      })
+    });
+
+  }*/
+  //console.log(this)
+   const players=[{"SS_id":1001,"SS_Name":"Session1","SS_Duration":112,"SS_Type":"V"},
   {"SS_id":1002,"SS_Name":"Session2","SS_Duration":60,"SS_Type":"V"},
   {"SS_id":1003,"SS_Name":"Session3","SS_Duration":15,"SS_Type":"P"},
   {"SS_id":1004,"SS_Name":"Session4","SS_Duration":120,"SS_Type":"V"},
   {"SS_id":1005,"SS_Name":"Session5","SS_Duration":15,"SS_Type":"P"}]
   console.log(players); 
+   /*const url="https://ibm-sprint.herokuapp.com/session";
+    const response= fetch(url);
+    const data= response.json();
+    console.log(data);
   /*fetch("https://ibm-sprint.herokuapp.com/session")
       .then(console.log())
   */
+ 
   const renderPlayer=(player,index)=>{
     return(
+
       <tr key={index}>
         <td>{player.SS_id}</td>
-        <td>{player.SS_Name}</td>
+        <a href={"https://ibm-sprint.herokuapp.com/content/"+player.SS_id}><td>{player.SS_Name}</td></a>
         <td>{player.SS_Duration}</td>
         <td>{player.SS_Type}</td>
       </tr>
     )
   }
+
   return (
+    
     <div className="App">
       <ReactBootStrap.Table striped bordered hover>
   <thead>
