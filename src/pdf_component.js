bdb8ae7477765b43ecaf1c49bd0629a3d869c8c8
Vibhor Singh <vibhor.formal@gmail.com>
2021-02-10 13:18:54 +0530

    Update

--- src/pdf_component.js
+++ src/pdf_component.js
@@ -4,10 +4,17 @@ function pdf({stateId}) {
  
  
     
-      return(<div>
-          <h1>PDF {stateId}</h1>
-          {console.log(stateId)}	
-     </div>
+      return(
+     <div >
+     <h4>PDF File</h4>
+
+   <embed src="http://www.africau.edu/images/default/sample.pdf" type="application/pdf" width="100%" height="700px" />
+     <hr />
+
+
+
+     <hr />
+   </div>
       )
      }
             
